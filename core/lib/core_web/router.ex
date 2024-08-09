8ab183965654224eae49a14101b9ff5e950f85fd
Adam Erikuly <adamerikoff@protonmail.com>
2024-08-09 18:23:34 +0500

    Update router.ex

--- core/lib/core_web/router.ex
+++ core/lib/core_web/router.ex
@@ -21,9 +21,9 @@ defmodule CoreWeb.Router do
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", CoreWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", CoreWeb do
+    pipe_through :api
+  end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:core, :dev_routes) do
